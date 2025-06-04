6ec248fe1323504b5deec32e58e3a9085c7bb9fc
Kaidan Theron <kaidantheron@Kaidans-MacBook-Air.local>
2025-06-04 12:05:24 +0200

    add utility to get creator id from post id

--- src/source/db/util.clj
+++ src/source/db/util.clj
@@ -1,7 +1,13 @@
 (ns source.db.util
   (:require [source.config :as conf]
             [next.jdbc :as jdbc]
-            [next.jdbc.result-set :as rs]))
+            [next.jdbc.result-set :as rs]
+            [source.db.master.outgoing-posts :as oposts]
+            [source.db.master.users :as users]
+            [source.db.master.bundles :as bundles]
+            [jsonista.core :as json]
+            [source.db.master.core :as master]
+            [malli.generator :as mg]))
 
 (def ^:private sqlite-config
   {:dbtype "sqlite"})
@@ -17,6 +23,14 @@
       (merge {:dbname (db-path (name dbname))})
       (jdbc/get-connection)
       (jdbc/with-options {:builder-fn rs/as-unqualified-lower-maps})))
+
+(defn creator-id
+  ([post-id]
+   (-> (conn :master)
+       (creator-id post-id)))
+  ([ds post-id]
+   (-> (oposts/select-outgoing-post-by-id ds {:id post-id})
+       (:creator_id))))
 
 (comment
   (db-path "master")
