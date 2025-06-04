a51612858f7b3efc910ba2ab131fcaa3758ec414
Kaidan Theron <kaidantheron@Kaidans-MacBook-Air.local>
2025-06-04 21:16:49 +0200

    update event category sql insertion queries

--- src/source/db/creator/sql/event_category.sql
+++ src/source/db/creator/sql/event_category.sql
@@ -2,7 +2,7 @@
 
 -- :name create-event-category-join-table :! :n
 -- :doc creates the event-category-join table
-create table if not exists event-category-join (
+create table if not exists event_category (
     id integer primary key autoincrement,
     event_id integer not null,
 		category_id text not null,
@@ -11,14 +11,14 @@ create table if not exists event-category-join (
 
 -- :name drop-event-category-table :! :n
 -- :doc drops the whole event-category table
-drop table if exists event-category
+drop table if exists event_category
 
 -- :name select-all-event-category :? :*
 -- :doc gets all records from event-category table
-select * from event-category
+select * from event_category
 
 -- :name insert-event-category :! :n
 -- :doc insert an event into the event-category table
-insert into event-category (:i*:cols) values (:v*:vals)
+insert into event_category (:i*:cols) values :t*:vals
 
 
