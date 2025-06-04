85eedd327d9018c060836e5e33ead9cdb5ae439f
Kaidan Theron <kaidantheron@Kaidans-MacBook-Air.local>
2025-06-04 16:28:25 +0200

    update setup db function in db master core

--- src/source/db/master/core.clj
+++ src/source/db/master/core.clj
@@ -10,7 +10,9 @@
             [source.db.master.cadences :as cadences]
             [source.db.master.categories :as categories]
             [source.db.master.feeds-categories :as feeds-categories]
-            [source.db.master.outgoing-posts :as oposts]))
+            [source.db.master.outgoing-posts :as oposts]
+            [source.db.master.feeds :as feeds]
+            [source.db.master.bundles :as bundles]))
 
 (declare table)
 (declare tables)
@@ -37,48 +39,18 @@
      (> 0))
     false))
 
-(defn setup-db [ds]
-  (when-not (table? ds "users")
-    (users/create-users-table ds))
-  (when-not (table? ds "cadences")
-    (cadences/create-cadences-table ds))
-  (when-not (table? ds "content_types")
-    (content-types/create-content-types-table ds))
-  (when-not (table? ds "providers")
-    (providers/create-providers-table ds))
-  (when-not (table? ds "baselines")
-    (baselines/create-baselines-table ds))
-  (when-not (table? ds "sectors")
-    (sectors/create-sectors-table ds))
-
-  (when-not (records? ds "cadences")
-    (seed-table ds {:table "cadences"
-                    :cols ["label" "days"]
-                    :vals [["daily" 1]
-                           ["weekly" 7]
-                           ["biweekly" 14]
-                           ["monthly" 30]]})
-    (seed-table ds {:table "baselines"
-                    :cols ["label" "min" "max"]
-                    :vals [["0-1000" 0 1000]
-                           ["1000-10000" 1000 10000]
-                           ["10000-100000" 10000 100000]
-                           ["100000-1000000" 100000 1000000]]})
-    (seed-table ds {:table "content_types"
-                    :cols ["name"]
-                    :vals [["video"]
-                           ["podcast"]
-                           ["blog"]]})
-    (seed-table ds {:table "providers"
-                    :cols ["name" "domain" "content_type_id"]
-                    :vals [["youtube" "www.youtube.com" 1]
-                           ["spotify" "www.spotify.com" 2]
-                           ["medium" "www.medium.com" 3]]})
-    (seed-table ds {:table "sectors"
-                    :cols ["name"]
-                    :vals [["renewable energy"]
-                           ["conservation ecology"]
-                           ["recycling"]]})))
+(def seeders
+  [{:name "cadences" :create cadences/create-cadences-table :seed true}
+   {:name "content_types" :create content-types/create-content-types-table :seed true}
+   {:name "providers" :create providers/create-providers-table :seed true}
+   {:name "baselines" :create baselines/create-baselines-table :seed true}
+   {:name "sectors" :create sectors/create-sectors-table :seed true}
+   {:name "categories" :create categories/create-table :seed true}
+   {:name "outgoing_posts" :create oposts/create-outgoing-posts-table :seed false}
+   {:name "feeds" :create feeds/create-table :seed false}
+   {:name "feeds_categories" :create feeds-categories/create-table :seed false}
+   {:name "users" :create users/create-users-table :seed false}
+   {:name "bundles" :create bundles/create-bundles-table :seed false}])
 
 (defn drop-tables [ds]
   (loop [tnames (table-names ds)]
@@ -87,14 +59,73 @@
         (drop-table ds {:table tname})
         (recur (vec (rest tnames)))))))
 
+(defn setup-db [ds seeders]
+  (drop-tables ds)
+  (doseq [{:keys [name create seed]} seeders]
+    (when-not (table? ds name)
+      (create ds))
+    (when (and seed (not (records? ds name)))
+      (->> name
+           (keyword)
+           (db.util/seed-data)
+           (db.util/apply-seed ds name)))))
+
 (defn get-post-categories [ds post-id]
   (let [feed-id (-> (oposts/select-outgoing-post-by-id ds {:id post-id})
-                    (:feed-id))]
+                    (:feed_id))]
     (feeds-categories/select-by-feed-id ds {:feed-id feed-id})))
 
 (comment
   (def ds (db.util/conn "master"))
-  (setup-db ds)
+  (setup-db ds seeders)
+  (users/insert-user ds {:email "merveillevaneck@gmail.com"
+                         :password "test"
+                         :firstname "merv"
+                         :lastname "ilicious"
+                         :business-name "modulr"
+                         :type "creator"})
+  (bundles/insert-bundle ds {:cols ["user_id"]
+                             :vals [1]})
+  (feeds/insert ds {:cols ["title"
+                           "rss_url"
+                           "user_id"
+                           "created_at"
+                           "content_type_id"
+                           "cadence_id"
+                           "baseline_id"]
+                    :vals ["AFEED"
+                           "sdfsf"
+                           1
+                           "sdfsfsf"
+                           1
+                           1
+                           1]})
+  (oposts/insert-outgoing-post ds {:bundle-id 1
+                                   :title "A bloody title"
+                                   :subtitle "subtitle"
+                                   :stream-url "sdfsf"
+                                   :content-type "video"
+                                   :feed-id 1
+                                   :creator-id 1})
+  (feeds-categories/insert-feeds-categories ds {:cols ["feed_id"
+                                                       "category_id"]
+                                                :vals [1
+                                                       1]})
+
+  (feeds-categories/insert-feeds-categories ds {:cols ["feed_id"
+                                                       "category_id"]
+                                                :vals [1
+                                                       2]})
+
+  (feeds-categories/insert-feeds-categories ds {:cols ["feed_id"
+                                                       "category_id"]
+                                                :vals [1
+                                                       3]})
+  (bundles/select-all-bundles ds)
+  (feeds/select-all ds)
+  (feeds-categories/select-all ds)
+  (oposts/select-outgoing-post-by-id ds {:id 1})
+  (get-post-categories ds 1)
   (drop-tables ds)
   (drop-table ds {:table "users"})
   (table? ds "users")
