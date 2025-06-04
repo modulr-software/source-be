5e841d60765bdaad052f5f41713bd94df7c70af2
Kaidan Theron <kaidantheron@Kaidans-MacBook-Air.local>
2025-06-04 16:31:53 +0200

    add feeds_categories to master db

---
+++ src/source/db/master/feeds_categories.clj
@@ -0,0 +1,11 @@
+(ns source.db.master.feeds-categories
+  (:require [hugsql.adapter.next-jdbc :as next-adapter]
+            [hugsql.core :as hugsql]))
+
+(declare create-table)
+(declare drop-table)
+(declare select-all)
+(declare insert-feeds-categories)
+(declare select-by-feed-id)
+(hugsql/def-db-fns "source/db/master/sql/feeds_categories.sql"
+  {:adapter (next-adapter/hugsql-adapter-next-jdbc)})
