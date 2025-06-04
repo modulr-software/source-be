78308b89f689540ee011d1e6b3709e27a0f3f42d
Kaidan Theron <kaidantheron@Kaidans-MacBook-Air.local>
2025-06-04 16:17:56 +0200

    add db-name util to db util

--- src/source/db/util.clj
+++ src/source/db/util.clj
@@ -4,10 +4,7 @@
             [next.jdbc.result-set :as rs]
             [source.db.master.outgoing-posts :as oposts]
             [source.db.master.users :as users]
-            [source.db.master.bundles :as bundles]
-            [jsonista.core :as json]
-            [source.db.master.core :as master]
-            [malli.generator :as mg]))
+            [source.db.master.bundles :as bundles]))
 
 (def ^:private sqlite-config
   {:dbtype "sqlite"})
@@ -32,6 +29,34 @@
    (-> (oposts/select-outgoing-post-by-id ds {:id post-id})
        (:creator_id))))
 
+(defn db-name
+  ([type]
+   (name type))
+  ([type id]
+   (str (name type) "_" id)))
+
 (comment
   (db-path "master")
-  (conn :master))
+  (conn :master)
+  (let [ds (conn :master)]
+    (users/create-users-table ds)
+    (bundles/create-bundles-table ds)
+    (oposts/create-outgoing-posts-table ds)
+    (bundles/insert-bundle ds {:cols ["user_id" "hash"] :vals [1 "test"]})
+    (users/insert-user ds {:email "merveillevaneck@gmail.com"
+                           :password "test"
+                           :firstname "merv"
+                           :lastname "ilicious"
+                           :business-name "modulr"
+                           :type "creator"})
+    (oposts/insert-outgoing-post ds {:bundle-id 1
+                                     :title "Best Video"
+                                     :stream-url "sdfsfd"
+                                     :content-type "video"
+                                     :subtitle "Test Subtitle"
+                                     :creator-id 1})
+
+    (println (creator-id ds 1))
+    (users/drop-users-table ds)
+    (bundles/drop-bundles-table ds)
+    (oposts/drop-outgoing-posts-table ds)))
