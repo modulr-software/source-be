4a806385d7b1a4bb9755c45e5e7a8933e9006284
Kaidan Theron <kaidantheron@Kaidans-MacBook-Air.local>
2025-06-04 16:39:25 +0200

    update sql for outgoing posts to have creator id

--- src/source/db/master/sql/outgoing_posts.sql
+++ src/source/db/master/sql/outgoing_posts.sql
@@ -9,8 +9,16 @@ create table if not exists outgoing_posts (
     subtitle text,
     stream_url text default null,
     content_type text default null,
+		feed_id integer,
+		creator_id integer,
+    foreign key (creator_id) references users(id),
     foreign key (bundle_id) references bundles(id)
+		foreign key (feed_id) references feeds(id)
 )
+
+-- :name select-outgoing-post-by-id :? :1
+-- :doc get the user by the given post id
+select * from outgoing_posts where id = :id
 
 -- :name select-outgoing-posts-by-bundle-id :? :*
 -- :doc gets all outgoing posts by bundle-id
@@ -18,8 +26,8 @@ select * from outgoing_posts where bundle_id = :bundle-id
 
 -- :name insert-outgoing-post :! :n
 -- :doc insert an outgoing post for a bundle id
-insert into outgoing_posts (bundle_id, title, subtitle, stream_url, content_type)
-values (:bundle-id, :title, :subtitle, :stream-url, :content-type)
+insert into outgoing_posts (bundle_id, title, subtitle, stream_url, content_type, creator_id, feed_id)
+values (:bundle-id, :title, :subtitle, :stream-url, :content-type, :creator-id, :feed-id)
 
 -- :name select-outgoing-posts-by-category :? :*
 -- :doc select a set of outgoing psots for a bundle id by the content type
