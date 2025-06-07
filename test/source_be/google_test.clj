1605ae4a689649505ac23051141ff5858d7254ed
Keagan <keaganncollins@gmail.com>
2025-06-07 17:10:36 +0200

    cleaned up google oauth2 flow implementation using cache protocol

--- test/source_be/google_test.clj
+++ test/source_be/google_test.clj
@@ -1,45 +1,20 @@
 (ns source-be.google-test
-  (:require [clojure.test :refer :all]))
+  (:require [clojure.test :refer :all]
+            [source.cache :as cache]
+            [source.oauth2.google :as google]
+            [clj-oauth2.client :as oauth2]))
+
+
+(deftest google-auth-flow-test
+  (testing "testing the google oauth2 flow"
+    (with-redefs [])
+    (let [auth-reqs-service (cache/create-cache)]
+      (google/-google-session-user )     
+      )))
+
 
 (deftest yeet-test
   (testing "adding numbers together"
     (with-redefs [])
     (is (= 69 (+ 60 9)))))
-
-;; TODO: DELETE ALL OF THE BELOW WHEN YOU ARE DONE IN OAUTH2/GOOGLE
-;; if this is in a different namespace
-(def ^:private cache (atom {}))
-
-(defn- -something-that-updates [*cache]
-  (swap! *cache assoc (random-uuid) {:some "value"}))
-
-(defn- -something-that-reads [*cache]
-  (let [cache' @*cache]
-    (swap! cache dissoc (second (last cache')))
-    (if (> (count cache') 3)
-      :success
-      :fail)))
-
-(comment
-  (def ^:private test-cache (atom {}))
-  (-something-that-updates test-cache)
-  @cache
-  (-something-that-reads test-cache))
-
-(defprotocol AuthRequestCache
-  (something-that-updates [this])
-  (something-that-reads [this]))
-
-(defn create-auth-request-cache [& _opts]
-  (let [lcache (atom {})]
-    (reify AuthRequestCache
-      (something-that-updates [_]
-        (-something-that-updates lcache))
-      (something-that-reads [_]
-        (-something-that-reads lcache)))))
-
-(def auth-request-cache (create-auth-request-cache))
-
-(something-that-updates auth-request-cache)
-
 
