bab05bbfcd4c7fa105c5cb4c9746d399de33e0e0
Keagan <keaganncollins@gmail.com>
2025-06-09 21:49:52 +0200

    updated CORS middleware to get origin from config

--- src/source/middleware/core.clj
+++ src/source/middleware/core.clj
@@ -1,6 +1,7 @@
 (ns source.middleware.core
   (:require [source.middleware.auth.core :as auth]
             [source.middleware.content-type :as content-type]
+            [source.config :as conf]
             [ring.middleware.cors :refer [wrap-cors]]
             [ring.middleware.params :refer [wrap-params]]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
@@ -10,10 +11,10 @@
 (defn apply-generic [app]
   (-> app
       (content-type/wrap-content-type)
-      (wrap-cors :access-control-allow-origin [#"http://localhost:3000"]
+      (wrap-cors :access-control-allow-origin [(re-pattern (conf/read-value :origin))]
                  :access-control-allow-methods [:get :put :post :delete])
       (wrap-params)
-      (wrap-defaults (assoc site-defaults :session false))
+      (wrap-defaults (assoc site-defaults :session false :security {:anti-forgery false}))
       (ring/wrap-json-response)
       (ring/wrap-json-body {:keywords? true})
       (cookies/wrap-cookies)))
