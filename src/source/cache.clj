286e4516aecb37d761b71fb2cf4d264358b8a9ed
Keagan <keaganncollins@gmail.com>
2025-06-11 19:37:22 +0200

    updated protocol to use overrides

--- src/source/cache.clj
+++ src/source/cache.clj
@@ -1,33 +1,26 @@
 (ns source.cache
   (:require [source.util :as util]))
 
-
 (defprotocol Cache
   (get-item [this uuid])
-  (add-item [this item])
-  (add-item-with-uuid [this item uuid])
+  (add-item [this item]
+    [this item uuid])
   (remove-item [this uuid])
   (get-all-items [this]))
-
 
 (defn- -get-item [*cache uuid]
   (get-in @*cache [uuid]))
 
-
 (defn- -remove-item [*cache uuid]
   (swap! *cache dissoc uuid))
 
-
-(defn- -add-item [*cache item & {:keys [uuid]}]
-  (let [uuid (or uuid (util/uuid))]
-    (swap! *cache assoc uuid item)
-    {:item item
-     :uuid uuid}))
-
+(defn- -add-item [*cache item uuid]
+  (swap! *cache assoc uuid item)
+  {:item item
+   :uuid uuid})
 
 (defn -get-all-items [*cache]
   @*cache)
-
 
 (defn create-cache []
   (let [lcache (atom {})]
@@ -37,17 +30,16 @@
       (remove-item [_ uuid]
         (-remove-item lcache uuid))
       (add-item [_ item]
-        (-add-item lcache item))
-      (add-item-with-uuid [_ item uuid]
-        (-add-item lcache item :uuid uuid))
+        (-add-item lcache item (util/uuid)))
+      (add-item [_ item uuid]
+        (-add-item lcache item uuid))
       (get-all-items [_]
         (-get-all-items lcache)))))
 
-
 (comment
   (def cache (create-cache))
-  (add-item-with-uuid cache "testitem" "a1b2c3d4e5f6")
-  (add-item-with-uuid cache "anothertestitem" "cheese")
+  (add-item cache "testitem" "a1b2c3d4e5f6")
+  (add-item cache "anothertestitem" "cheese")
   (add-item cache "yetanothertestitem")
   (get-item cache "a1b2c3d4e5f6")
   (remove-item cache "a1b2c3d4e5f6")
--- test/source_be/cache_test.clj
+++ test/source_be/cache_test.clj
@@ -7,10 +7,10 @@
     (let [google-test-cache (cache/create-cache)
           emails-test-cache (cache/create-cache)
           google-result (do
-                          (cache/add-item-with-uuid google-test-cache {:uri "http://yeet.com/test"} "testuuid")
+                          (cache/add-item google-test-cache {:uri "http://yeet.com/test"} "testuuid")
                           (cache/get-item google-test-cache "testuuid"))
           emails-result (do
-                          (cache/add-item-with-uuid emails-test-cache "test@toast.com" "testuuid")
+                          (cache/add-item emails-test-cache "test@toast.com" "testuuid")
                           (cache/get-item emails-test-cache "testuuid"))]
 
       (is (= (count (cache/get-all-items google-test-cache)) 1))
@@ -18,11 +18,11 @@
       (is (= google-result {:uri "http://yeet.com/test"}))
       (is (= emails-result "test@toast.com"))
 
-      (cache/add-item-with-uuid google-test-cache {:uri "http://delete.com/"} "todelete")
+      (cache/add-item google-test-cache {:uri "http://delete.com/"} "todelete")
       (cache/add-item google-test-cache {:uri "http://dontdelete.com/"})
       (is (= (count (cache/get-all-items google-test-cache)) 3))
 
-      (cache/add-item-with-uuid emails-test-cache "delete@delete.com" "todelete")
+      (cache/add-item emails-test-cache "delete@delete.com" "todelete")
       (cache/add-item emails-test-cache "dontdelete@dontdelete.com")
       (is (= (count (cache/get-all-items emails-test-cache)) 3))
 
