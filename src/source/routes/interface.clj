58f2042b64625648937d50ca440728bdd4073487
Merveille van Eck <merveillevaneck@gmail.com>
2025-06-24 19:56:52 +0200

    removed all route handler from routes interface, added create-app

--- src/source/routes/interface.clj
+++ src/source/routes/interface.clj
@@ -1,28 +1,6 @@
 (ns source.routes.interface
-  (:require [source.routes.user :as user]
-            [source.routes.users :as users]
-            [source.routes.login :as login]
-            [source.routes.authorized :as authorized]
-            [source.routes.register :as register]
-            [source.routes.add-admin :as add-admin]))
+  (:require [source.routes.reitit :as reitit]))
 
-(defn users [request]
-  (users/handler request))
+(defn create-app []
+  (reitit/create-app))
 
-(defn user [request]
-  (user/get-handler request))
-
-(defn update-user [request]
-  (user/patch-handler request))
-
-(defn login [request]
-  (login/handler request))
-
-(defn register [request]
-  (register/handler request))
-
-(defn authorized [request]
-  (authorized/handler request))
-
-(defn add-admin [request]
-  (add-admin/handler request))
