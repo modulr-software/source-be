9484e7de3a3e246e49ab3e345673251cc9a7cd2f
Merveille van Eck <merveillevaneck@gmail.com>
2025-06-24 19:55:11 +0200

    changed login handler name

--- src/source/routes/login.clj
+++ src/source/routes/login.clj
@@ -4,17 +4,17 @@
             [source.services.users :as users]
             [source.password :as pw]))
 
-(defn handler [{:keys [ds body] :as _request}]
+(defn post [{:keys [ds body] :as _request}]
   (let [{:keys [email password]} body
         user (users/user ds {:where [:= :email email]})]
     (if
-      (or (not (pw/verify-password password (:password user)))
-          (not (some? user)))
+     (or (not (pw/verify-password password (:password user)))
+         (not (some? user)))
       {:status 401 :body {:message "Invalid username or password!"}}
 
       (res/response (auth/login ds {:user user})))))
 
-(comment 
+(comment
   (require '[source.db.interface :as db])
-  (handler {:ds (db/ds :master) :body {:email "toast@toast.com" :password "poop"}})
+  (post {:ds (db/ds :master) :body {:email "toast@toast.com" :password "poop"}})
   ())
