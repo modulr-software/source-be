c8e0ca29c49ed5e114505c91dbdd2d4f3ab56beb
Keagan <keaganncollins@gmail.com>
2025-06-26 14:28:04 +0200

    added todo

--- src/source/services/analytics.clj
+++ src/source/services/analytics.clj
@@ -1,7 +1,7 @@
 (ns source.services.analytics
   (:require [source.db.interface :as db]))
 
-; this needs to be updated to return the added event as well
+; TODO: this needs to return the added event as well
 (defn insert-event! [ds event]
   (->> {:tname :analytics
         :data event}
