efe4401ce8935a9d65dc979b479879867b519521
Keagan <keaganncollins@gmail.com>
2025-06-26 14:26:54 +0200

    replaced original core with reitit

--- src/source/routes/core.clj
+++ src/source/routes/core.clj
@@ -1,48 +1,92 @@
 (ns source.routes.core
-  (:require [compojure.core :refer [routes GET POST PATCH]]
-            [compojure.route :as route]
-            [source.middleware.auth.util :as auth.util]
+  (:require [reitit.ring :as ring]
             [source.middleware.interface :as mw]
+            [source.db.interface :as db]
+            [clojure.data.json :as json]
+            [source.routes.user :as user]
             [source.routes.users :as users]
-            [source.routes.google :as google]
-            [source.routes.protected :as protected]
-            [source.routes.admin :as admin]))
-
-(defn home [_req]
-  {:status 200
-   :body {:value "bye bitch"}
-   :cookies {"test-session" {:value "test"}}})
+            [source.routes.login :as login]
+            [source.routes.register :as register]
+            [source.routes.google-launch :as google-launch]
+            [source.routes.google-redirect :as google-redirect]
+            [source.routes.admin :as admin]
+            [source.routes.authorized :as authorized]))
 
 (defn create-app []
-  (mw/apply-generic
-   (routes
-    (GET "/" req [] (home req))
-
-    (POST "/login" req [] (users/login req))
-    (POST "/register" req [] (users/register req))
-    (GET "/users" req [] (users/users req))
-    (PATCH "/users/:id" req [] (users/update-user req))
-
-    (GET "/oauth2/google" req [] (google/google-launch req))
-    (GET "/oauth2/google/callback" req [] (google/google-redirect req))
-
-    (->
-     (routes
-      (GET "/authorized" req [] (protected/authorized req)))
-     (mw/apply-auth))
-
-    (->
-     (routes
-      (POST "/add-admin" req [] (admin/add-admin req)))
-     (mw/apply-auth {:required-type :admin}))
-
-    (route/not-found "Page not found"))))
+  (let [ds (db/ds :master)]
+    (ring/ring-handler
+     (ring/router
+      [["/" {:middleware [[mw/apply-generic :ds ds]]}
+        ["" (fn [_request] {:status 200 :body {:message "success"}})]
+        ["users"
+         ["" users/handler]
+         ["/:id" {:get user/get
+                  :patch user/patch}]]
+        ["login" {:post login/post}]
+        ["register" {:post register/post}]
+        ["oauth2"
+         ["/google"
+          ["" {:get google-launch/get}]
+          ["/callback" {:get google-redirect/get}]]]
+        ["protected" {:middleware [[mw/apply-auth]]}
+         ["/authorized" {:get authorized/get}]]
+        ["admin" {:middleware [[mw/apply-auth {:required-type :admin}]]}
+         ["/add-admin" {:post admin/post}]]]]))))
 
 (comment
-  (let [app (create-app)
-        req {:uri "/authorized"
-             :request-method :get
-             :headers {"authorization"
-                       (str "Bearer " (auth.util/sign-jwt {:id 1}))}}]
-    (app req)))
+  (require '[source.middleware.auth.util :as auth.util])
 
+  (let [app (create-app)
+        request {:uri "/users" :request-method :get}]
+    (-> request
+        app
+        :body
+        (json/read-json {:key-fn keyword})))
+
+  (let [app (create-app)
+        request {:uri "/users/3" :request-method :get}]
+    (-> request
+        app
+        :body
+        (json/read-json {:key-fn keyword})))
+
+  (let [app (create-app)
+        request {:uri "/users/3"
+                 :request-method :patch
+                 :body {:firstname "Keagan"
+                        :lastname "Collins"}}]
+    (-> request
+        app
+        :body
+        (json/read-json {:key-fn keyword})))
+
+  (let [app (create-app)
+        request {:uri "/protected/authorized"
+                 :request-method :get
+                 :headers {"authorization" (str "Bearer " (auth.util/sign-jwt {:id 5 :type "distributor"}))}}]
+    (-> request
+        app
+        :body
+        (json/read-json {:key-fn keyword})))
+
+  (let [app (create-app)
+        request {:uri "/admin/add-admin"
+                 :request-method :post
+                 :body {:email "test@test.com"
+                        :password "test"
+                        :confirm-password "test"}
+                 :headers {"authorization" (str "Bearer " (auth.util/sign-jwt {:id 1 :type "admin"}))}}]
+    (-> request
+        app
+        :body
+        (json/read-json {:key-fn keyword})))
+
+  (let [app (create-app)
+        request {:uri "/oauth2/google"
+                 :request-method :get}]
+    (-> request
+        app 
+        :body 
+        (json/read-json {:key-fn keyword})))
+
+  ())
