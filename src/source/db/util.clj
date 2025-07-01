260dd4bd64acb9ac397afa280025ef3f7acb7552
Keagan <keaganncollins@gmail.com>
2025-07-01 10:06:37 +0200

    updated db util conn

--- src/source/db/util.clj
+++ src/source/db/util.clj
@@ -6,15 +6,25 @@
 (defn db-path [dbname]
   (str (conf/read-value :database :dir) dbname))
 
-(defn conn [dbname]
-  (-> {:dbtype (conf/read-value :database :type)}
-      (merge {:dbname (db-path (name dbname))})
-      (jdbc/get-connection)
-      (jdbc/with-options {:builder-fn rs/as-unqualified-lower-maps})))
-
 (defn db-name
   ([type]
    (name type))
   ([type id]
    (str (name type) "_" id)))
+
+(defn db-conn [dbname]
+  (-> {:dbtype (conf/read-value :database :type)}
+       (merge {:dbname (db-path dbname)})
+       (jdbc/get-connection)
+       (jdbc/with-options {:builder-fn rs/as-unqualified-lower-maps})))
+
+(defn conn
+  ([]
+   (conn :master))
+  ([db-type]
+   (assert (= db-type :master))
+   (db-conn (db-name db-type)))
+  ([db-type id]
+   (assert (not (= db-type :master)))
+   (db-conn (db-name db-type id))))
 
