44e706ee18b70c9c0b910f8675f7d2829c2cd327
Keagan <keaganncollins@gmail.com>
2025-07-29 19:27:48 +0200

    allow for datastore creation and deletion in migration

--- src/source/datastore/util.clj
+++ src/source/datastore/util.clj
@@ -9,14 +9,48 @@
       (clojure.string/replace "/." "/")
       (str path)))
 
-(defn store-path [store-name]
-  (-> (str (conf/read-value :database :dir) store-name)
-      (absolute)))
+(defn store-path
+  ([store-name]
+   (-> (str (conf/read-value :database :dir) store-name)
+       (absolute))))
 
 (defn config [store-name]
   {:store {:backend :file
            :path (store-path (name store-name))}
-   :schema-flexibility :read})
+   :schema-flexibility :read
+   :initial-tx [{:db/ident        :selection-schemas/id
+                 :db/valueType    :db.type/long
+                 :db/cardinality  :db.cardinality/one
+                 :db/unique       :db.unique/identity}
+                {:db/ident        :selection-schemas/schema
+                 :db/valueType    :db.type/any
+                 :db/cardinality  :db.cardinality/one}
+
+                {:db/ident        :output-schemas/id
+                 :db/valueType    :db.type/long
+                 :db/cardinality  :db.cardinality/one
+                 :db/unique       :db.unique/identity}
+                {:db/ident        :output-schemas/schema
+                 :db/valueType    :db.type/any
+                 :db/cardinality  :db.cardinality/one}
+
+                {:db/ident        :providers/id
+                 :db/valueType    :db.type/long
+                 :db/cardinality  :db.cardinality/one
+                 :db/unique       :db.unique/identity}
+                {:db/ident        :providers/name
+                 :db/valueType    :db.type/string
+                 :db/cardinality  :db.cardinality/one}]})
+
+(defn create-datastore [store-name]
+  (when-not (d/database-exists? (config store-name))
+    (println "Creating datastore...")
+    (d/create-database (config store-name))))
+
+(defn delete-datastore [store-name]
+  (when (d/database-exists? (config store-name))
+    (println "Deleting datastore...")
+    (d/delete-database (config store-name))))
 
 (defn conn
   "Open a connection to a datahike store"
