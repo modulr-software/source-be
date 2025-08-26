85ab28b4c4141cd64b0b73f755bc87f0d1fea088
Keagan <keaganncollins@gmail.com>
2025-08-26 14:50:09 +0200

    updated server to use a component system

--- src/source/server.clj
+++ src/source/server.clj
@@ -7,57 +7,97 @@
             [source.routes.interface :as routes]
             [source.util :as util]))
 
-(defonce ^:private *server (atom nil))
-
 (defonce ^:private *components (atom nil))
 
-(defn initialise-components! []
-  (if (some? @*components)
-    (do
-      (println "Components already initialised.")
-      (let [{:keys [ds store]} @*components]
-        (->> (jobs/interrupted-jobs ds store)
-             (congest/create-job-service)
-             (swap! *components assoc :js))))
+(defn initialise-server! [{:keys [ds store js]}]
+  (http/run-server
+   (routes/create-app {:ds ds
+                       :store store
+                       :js js})
+   {:port 3000}))
 
-    (try
-      (println "Initialising components...")
-      (let [{:keys [ds store]} (reset! *components {:ds (db/ds :master)
-                                                    :store (store/ds :datahike)})]
-        (->> (jobs/interrupted-jobs ds store)
-             (congest/create-job-service)
-             (swap! *components assoc :js)))
-      (catch Exception e (println "Failed to initialise components: " (.getMessage e))))))
+(defn initialise-job-service! [{:keys [ds store] :as _deps}]
+  (->> (jobs/interrupted-jobs ds store)
+       (congest/create-job-service)))
+
+(defn component-on? [component]
+  (if (some? (get @*components component))
+    true
+    false))
+
+(defn deps-on? [deps]
+  (every? component-on? deps))
+
+(defn initialise!
+  "executes the init-fn on the provided component and, if successful, updates the components atom with the new component"
+  [{:keys [name init-fn deps] :as _component}]
+  (try
+    (when (deps-on? deps)
+      (swap! *components assoc name (init-fn @*components)))
+    (catch Exception e (println (str "Failed to initialise " name ":") e))))
+
+(defn initialise-components! [components]
+  (run! initialise! components))
 
 (defn running? []
-  (some? @*server))
+  (some? (:server @*components)))
 
 (defn start-server []
-  (cond (not (some? @*server))
+  (cond (not (some? (:server @*components)))
         (do
           (println "Starting server on port 3000...")
-          (initialise-components!)
-          (reset! *server (http/run-server
-                           (routes/create-app @*components)
-                           {:port 3000})))
+          (initialise-components! [{:name :ds
+                                    :init-fn (fn [_deps] (db/ds :master))
+                                    :deps []}
+                                   {:name :store
+                                    :init-fn (fn [_deps] (store/ds :datahike))
+                                    :deps []}
+                                   {:name :js
+                                    :deps [:ds :store]
+                                    :init-fn initialise-job-service!}
+                                   {:name :server
+                                    :deps [:ds :store :js]
+                                    :init-fn initialise-server!}]))
         :else
         (println "Server already running!")))
 
 (defn stop-server []
   (println "Stopping server...")
-  (when (some? @*components)
+  (when (some? (:js @*components))
     (congest/kill! (:js @*components)))
-  (when (some? @*server)
-    (@*server))
-  (reset! *server nil))
+  (when (some? (:server @*components))
+    (let [server (:server @*components)]
+      (server)))
+  (reset! *components nil))
 
-(defn restart-server []
-  (stop-server)
-  (start-server))
+(defn restart-server [& {:keys [keep-js]}]
+  (if keep-js
+    (do
+      (when (some? (:server @*components))
+        (let [server (:server @*components)]
+          (server)))
+      (swap! *components select-keys [:js])
+      (initialise-components! [{:name :ds
+                                :init-fn (fn [_deps] (db/ds :master))
+                                :deps []}
+                               {:name :store
+                                :init-fn (fn [_deps] (store/ds :datahike))
+                                :deps []}
+                               {:name :server
+                                :deps [:ds :store :js]
+                                :init-fn initialise-server!}]))
+    (do
+      (stop-server)
+      (start-server))))
 
 (comment
+  (start-server)
+  (stop-server)
+  (restart-server)
+  (restart-server :keep-js true)
   (def test-wrapper
     (util/wrap-json (fn [request] request)))
   (test-wrapper {:status 200
                  :body "{\"value\":\"Hello, Source!\"}"
-                 :headers {"Content-Type" "application/json"}}))
+                 :headers {"Content-Type" "application/json"}})
+  ())
