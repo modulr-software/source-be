63791861a4caa0429d6a5c18505d08cc1386da2b
Keagan <keaganncollins@gmail.com>
2025-10-10 13:59:08 +0200

    updated bundle posts endpoint to allow for filtering by content type and categories

--- src/source/routes/bundle_posts.clj
+++ src/source/routes/bundle_posts.clj
@@ -4,12 +4,14 @@
             [clojure.walk :as walk]
             [ring.util.response :as res]))
 
-(defn get
+(defn post
   {:summary "get all outgoing posts in the uuid-authorized bundle"
-   :parameters {:query [:map
+   :parameters {:body [:map [:category-ids [:vector :int]]]
+                :query [:map
                         [:uuid :string]
                         [:limit {:optional true} :int]
-                        [:start {:optional true} :int]]}
+                        [:start {:optional true} :int]
+                        [:type {:optional true} :int]]}
    :responses {200 {:body [:vector
                            [:map
                             [:id :int]
@@ -28,9 +30,35 @@
                             [:posted-at [:maybe :string]]]]}
                404 {:boy [:map [:message :string]]}}}
 
-  [{:keys [bundle-id query-params] :as _request}]
+  [{:keys [ds bundle-id query-params body] :as _request}]
   (let [bundle-ds (db.util/conn :bundle bundle-id)
-        {:keys [limit start]} (walk/keywordize-keys query-params)]
-    (res/response (services/outgoing-posts bundle-ds {:where (when start [:>= :id start])
-                                                      :limit limit
-                                                      :order-by [[:id :asc]]}))))
+        {:keys [limit start type]} (walk/keywordize-keys query-params)
+        {:keys [category-ids]} body
+
+        content-type-comp (when type [:= :content-type-id type])
+        start (when start (try (Integer/parseInt start) (catch Exception _)))
+        limit (when limit (try (Integer/parseInt limit) (catch Exception _)))
+
+        filtered-posts (services/outgoing-posts bundle-ds {:where content-type-comp})
+
+        categorised-posts (vec (if (seq category-ids)
+                                 (->> filtered-posts
+                                      (mapv
+                                       (fn [post]
+                                         (when (some (set category-ids) (->> {:feed-id (:feed-id post)}
+                                                                             (services/categories-by-feed ds)
+                                                                             (mapv :id)))
+                                           post)))
+                                      (remove nil?))
+                                 filtered-posts))
+
+        valid-start? (and (some? start) (>= start 0) (< start (count categorised-posts)))
+        started-posts (if valid-start?
+                        (subvec categorised-posts start)
+                        categorised-posts)
+
+        limited-posts (if (and (some? limit) (> (count started-posts) limit))
+                        (subvec started-posts 0 limit)
+                        started-posts)]
+
+    (res/response limited-posts)))
