49e5737d992ce78d0e8b00a733ae971135d7bac2
Keagan <keaganncollins@gmail.com>
2025-10-20 19:45:32 +0200

    updated categories table with display picture field

--- src/source/routes/categories.clj
+++ src/source/routes/categories.clj
@@ -1,13 +1,15 @@
 (ns source.routes.categories
   (:require [source.services.interface :as services]
-            [ring.util.response :as res]))
+            [ring.util.response :as res]
+            [source.db.util :as db.util]))
 
 (defn get
   {:summary "get all categories"
    :responses {200 {:body [:vector
                            [:map
                             [:id :int]
-                            [:name :string]]]}}}
+                            [:name :string]
+                            [:display-picture {:optional true} [:maybe :string]]]]}}}
   [{:keys [ds] :as _request}]
   (->> (services/categories ds)
        (res/response)))
