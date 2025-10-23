c4f0e3669e2ce6528708e1a0c7007a32abffd3b1
Keagan <keaganncollins@gmail.com>
2025-10-23 15:02:24 +0200

    updated bundle feeds route to filter by content type and categories

--- src/source/routes/bundle_feeds.clj
+++ src/source/routes/bundle_feeds.clj
@@ -1,11 +1,15 @@
 (ns source.routes.bundle-feeds
   (:require [ring.util.response :as res]
             [source.services.interface :as services]
-            [source.db.util :as db.util]))
+            [source.db.util :as db.util]
+            [clojure.walk :as walk]))
 
-(defn get
+(defn post
   {:summary "get all feeds present in the bundle authorised by uuid"
-   :parameters {:query [:map [:uuid :string]]}
+   :parameters {:query [:map
+                        [:uuid :string]
+                        [:type {:optional true} :int]]
+                :body [:map [:category-ids [:vector :int]]]}
    :responses {200 {:body [:vector
                            [:map
                             [:id :int]
@@ -24,7 +28,22 @@
                             [:state [:enum "live" "not live" "pending"]]]]}
                404 {:body [:map [:message :string]]}}}
 
-  [{:keys [ds bundle-id] :as _request}]
+  [{:keys [ds bundle-id query-params body] :as _request}]
   (let [bundle-ds (db.util/conn :bundle bundle-id)
-        feed-ids (mapv :feed-id (services/outgoing-posts bundle-ds))]
-    (res/response (services/feeds ds {:where [:in :id feed-ids]}))))
+        {:keys [category-ids]} body
+        {:keys [type]} (walk/keywordize-keys query-params)
+        feed-ids (mapv :feed-id (services/outgoing-posts bundle-ds))
+        category-filtered-feed-ids (if (empty? category-ids)
+                                     feed-ids
+                                     (->> {:where [:and
+                                                   [:in :feed-id feed-ids]
+                                                   [:in :category-id category-ids]]}
+                                          (services/feed-categories ds)
+                                          (mapv :feed-id)))
+        id-comp [:in :id category-filtered-feed-ids]
+        where-clause (if (some? type)
+                       [:and id-comp [:= :content-type-id type]]
+                       id-comp)
+        type-filtered (services/feeds ds {:where where-clause})]
+
+    (res/response type-filtered)))
