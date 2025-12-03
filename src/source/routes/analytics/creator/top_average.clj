9d597b8ff473985a5d567db8ceaf43ba3958c51d
Keagan <keaganncollins@gmail.com>
2025-12-03 13:41:51 +0200

    added average endpoints to get average engagement on creator and distributor side

---
+++ src/source/routes/analytics/creator/top_average.clj
@@ -0,0 +1,23 @@
+(ns source.routes.analytics.creator.top-average
+  (:require [source.services.analytics.interface :as analytics]
+            [ring.util.response :as res]
+            [source.util :as utils]))
+
+(defn get
+  {:summary "Get the average engagement (clicks and views) for a creator, optionally filtered by content type."
+   :parameters {:query [:map
+                        [:mindate :string]
+                        [:maxdate :string]
+                        [:contenttype {:optional true} [:maybe :int]]]}
+   :responses {200 {:body [:map [:average :float]]}}}
+
+  [{:keys [ds user query-params] :as _request}]
+  (let [{:keys [data success error]} (utils/validate get query-params :query)
+        {:keys [mindate maxdate contenttype]} data]
+    (if success
+      (let [result (analytics/average-engagement ds mindate maxdate {:creator-id (:id user)
+                                                                     :content-type-id contenttype})]
+        (res/response {:average result}))
+
+      (-> (res/response error)
+          (res/status 400)))))
