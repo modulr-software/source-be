27ede70131fb97bef0ee120462bcc1bb4cf16b4d
Keagan <keaganncollins@gmail.com>
2025-12-12 11:28:40 +0200

    fixed migration alter table

--- src/source/migrations/007_business_fields.clj
+++ src/source/migrations/007_business_fields.clj
@@ -2,13 +2,15 @@
   (:require [source.db.master]
             [source.db.honey :as hon]
             [source.db.tables :as tables]
-            [honey.sql.helpers :as hsql]))
+            [honey.sql.helpers :as hsql]
+            [source.db.util :as db.util]))
 
-(def business-types
-  [{:name "Independent"}
-   {:name "Commercial"}
-   {:name "Non-profit"}
-   {:name "State-owned"}])
+(def business-types-seed
+  {:tname :business-types
+   :data [{:name "Independent"}
+          {:name "Commercial"}
+          {:name "Non-profit"}
+          {:name "State-owned"}]})
 
 (defn run-up! [context]
   (let [ds-master (:db-master context)]
@@ -17,10 +19,16 @@
      :source.db.master
      :business-types)
 
+    (hon/insert! ds-master business-types-seed)
+
     (hon/execute!
      ds-master
      (-> (hsql/alter-table :businesses)
-         (hsql/add-column :business-type-id :integer)
+         (hsql/add-column :business-type-id :integer)))
+
+    (hon/execute!
+     ds-master
+     (-> (hsql/alter-table :businesses)
          (hsql/add-column :registration :text)))))
 
 (defn run-down! [context]
