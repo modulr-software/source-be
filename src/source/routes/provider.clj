6e69f20b59703d9132702ec4a6e5932ed60874ab
Keagan <keaganncollins@gmail.com>
2025-12-17 12:03:21 +0200

    added and updated provider routes

--- src/source/routes/provider.clj
+++ src/source/routes/provider.clj
@@ -9,20 +9,39 @@
    :responses {200 {:body [:map
                            [:id :int]
                            [:name :string]
-                           [:domain :string]
-                           [:content-type-id :int]]}}}
+                           [:domain [:maybe :string]]
+                           [:content-type-id :int]
+                           [:instructions [:maybe :string]]
+                           [:placeholder-url [:maybe :string]]]}}}
 
   [{:keys [ds path-params] :as _request}]
   (->> path-params
        (services/provider ds)
        (res/response)))
 
-(defn delete 
+(defn post
+  {:summary "update provider by id"
+   :parameters {:path [:map [:id {:title "id"
+                                  :description "provider id"} :int]]
+                :body [:map
+                       [:name :string]
+                       [:domain {:optional true} [:maybe :string]]
+                       [:content-type-id :int]
+                       [:instructions {:optional true} [:maybe :string]]
+                       [:placeholder-url {:optional true} [:maybe :string]]]}
+   :responses {200 {:body [:map [:message :string]]}}}
+
+  [{:keys [ds path-params body] :as _request}]
+  (services/update-provider! ds {:id (:id path-params)
+                                 :data body})
+  (res/response {:message "successfully updated provider"}))
+
+(defn delete
   {:summary "given a provider id, delete provider and associated selection schemas"
    :parameters {:path [:map [:id {:title "id"
                                   :description "provider id"} :int]]}
    :responses {200 {:body [:map [:message :string]]}}}
-  
+
   [{:keys [store ds path-params] :as _request}]
   (->> path-params
        (services/delete-provider! ds))
