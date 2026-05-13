4f0b8099d1351f550d9be235597dd9161d6d5f42
Keagan <keaganncollins@gmail.com>
2026-05-13 09:11:05 +0200

    removed verbose archive logs

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ mkdir source-be-deploy
 cp start.sh migrate.sh source-be.service deps.edn target/source-be-standalone.jar source-be-deploy
 cp -r resources src source-be-deploy
 cp prod.env source-be-deploy/.env
-tar -czvf source-be-deploy.tar.gz ./source-be-deploy
+tar -czf source-be-deploy.tar.gz ./source-be-deploy
 
 echo "Copying archive..."
 sshpass -e scp -r /home/merv/Developer/source-be-deploy/source-be-deploy.tar.gz deploy@api.wearesource.earth:/home/deploy
@@ -23,7 +23,7 @@ echo "Done!"
 echo "Setting up prod server..."
 sshpass -e ssh deploy@api.wearesource.earth "
 	echo "Extracting archive..."
-	tar -xvzf source-be-deploy.tar.gz
+	tar -xzf source-be-deploy.tar.gz
 
 	echo "Restarting service..."
 	sudo systemctl restart source-fe.service
